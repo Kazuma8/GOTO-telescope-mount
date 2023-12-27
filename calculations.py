ea2355e3a3ee1ed60fb56ef6b0bc267bd49cd0e1
Kazuma8 <kazumasa3miura@gmail.com>
2023-12-27 18:38:15 +0000

    starting fresh, fixed planet function

--- calculations.py
+++ calculations.py
@@ -1,6 +1,7 @@
-from skyfield.api import Star, load
+from skyfield.api import Star, load, Loader
 from skyfield.data import hipparcos
 
+load = Loader('./data')
 
 def hipparcos_search(hip_number):
 
@@ -8,17 +9,21 @@ def hipparcos_search(hip_number):
         df = hipparcos.load_dataframe(f)
 
     star = Star.from_dataframe(df.loc[hip_number])
-    ra, dec = observe(star)
+    ra, dec = calculate_ra_dec(star)
     return ra, dec
 
 
-def observe(target):
+def calculate_ra_dec(target): #can parse class star, or planet as string
 
-    planets = load('de421.bsp')
+    planets_names = ['mercury', 'venus', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune', 'pluto']
+    large_planet_moon_ratio = ['mars', 'jupiter', 'saturn', 'uranus', 'neptune', 'pluto']
+    planets = load('de440.bsp')
     earth = planets['earth']
 
-    if target in planets:
-        target = planets[str(target)]
+    if target in planets_names:
+        if target in large_planet_moon_ratio: #if planet only has barycentre data
+            target = target + ' barycenter'
+        target = planets[target]
 
     ts = load.timescale()
     t = ts.now()
@@ -28,4 +33,4 @@ def observe(target):
 
 if __name__ == "__main__":
     #print(hipparcos_search(87937))
-    print(observe("mars"))
+    print(calculate_ra_dec("mars"))
